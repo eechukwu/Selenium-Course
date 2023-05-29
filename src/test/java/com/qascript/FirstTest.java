581db21a8459f86b49b1a8076fa5ed4ceb4e144b
Emmanuel C <eechukwu@gmail.com>
2023-05-29 14:53:48 +0100

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -8,21 +8,22 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FirstTest {
 
-
     @Test
-    public void OpenBrowser()  {
+    public void OpenBrowser() {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+
+        try {
+            driver.get("https://www.google.com");
+            System.out.println("Title of the page is: " + driver.getTitle());
+            Assert.assertTrue("Page title is not correct", driver.getTitle().equals("Google"));
+        } finally {
+            driver.quit();
+        }
     }
-
-
 }
-
 
