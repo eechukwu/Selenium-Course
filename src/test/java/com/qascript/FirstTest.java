b48ffd8d94fa481ff806ba5e93ad83de25bad274
Emmanuel C <eechukwu@gmail.com>
2023-05-29 17:00:12 +0100

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class PageTitleChecker {
+public class FirstTest {
     public static void main(String[] args) {
         // Set the path to the chromedriver executable
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
